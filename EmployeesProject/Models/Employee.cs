b854745db54edce317a71b5be1fdf6fbb03bc35c
Edgar Sokol <edgar.sokol@gmail.com>
2024-08-21 12:47:34 +0300

    working version

--- EmployeesProject/Models/Employee.cs
+++ EmployeesProject/Models/Employee.cs
@@ -1,7 +1,12 @@
-﻿namespace EmployeesProject.Models
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace EmployeesProject.Models
 {
     public class Employee
     {
+        [BsonId]
+        public ObjectId MongoId { get; set; }
         public string Name { get; set; }
         public string SurName { get; set; }
         public int Age { get; set; }
--- EmployeesProject/Program.cs
+++ EmployeesProject/Program.cs
@@ -3,6 +3,7 @@ using EmployeesProject.Models;
 using EmployeesProject.Repositories;
 using EmployeesProject.Services;
 using MongoDB.Driver;
+using System.Diagnostics;
 
 namespace EmployeesProject
 {
@@ -11,14 +12,87 @@ namespace EmployeesProject
         public static void Main(string[] args)
         {
             IEmployeesService employeesService = SetupDependencies();
-            /*            ProductionWorker newPWorker = new ProductionWorker();
-                        newPWorker.Name = "Rayko";
-                        newPWorker.SurName = "Jugger";
-                        newPWorker.Age = 25;
-                        newPWorker.Shift = "Night";
-                        employeesService.InsertProductionWorker(newPWorker);*/
-
-            employeesService.GetProductionWorkersList();
+            while (true)
+            {
+                Console.WriteLine("1. Office Workers Menu");
+                Console.WriteLine("2. Production Workers Menu");
+                Console.WriteLine("0. Exit Program");
+                string mainMenu = Console.ReadLine();
+                switch (mainMenu)
+                {
+                    case "0":
+                        //Exit Program
+                        return;
+                    case "1":
+                        Console.WriteLine("1. Office Workers List");
+                        Console.WriteLine("2. Add OfficeWorker");
+                        string choiceOfficeWorkers = Console.ReadLine();
+                        switch (choiceOfficeWorkers)
+                        {
+                            case "0":
+                                //Main Menu
+                                break;
+                            case "1":
+                                //OfficeWorker
+                                var OfficeWorkerList = employeesService.GetOfficeWorkersList();
+                                foreach (OfficeWorker officeWorker in OfficeWorkerList.Result)
+                                {
+                                    Console.WriteLine(officeWorker);
+                                }
+                                break;
+                            case "2":
+                                //InsertOfficeWorker
+                                OfficeWorker newOfficeWorker = new OfficeWorker();
+                                Console.WriteLine("Enter OfficeWorker name");
+                                string name = Console.ReadLine();
+                                Console.WriteLine("Enter OfficeWorker surName");
+                                string surName = Console.ReadLine();
+                                Console.WriteLine("Enter OfficeWorker age");
+                                int age = int.Parse(Console.ReadLine());
+                                Console.WriteLine("Enter OfficeWorker department");
+                                string department = Console.ReadLine();
+                                newOfficeWorker = new OfficeWorker(name, surName, age, department);
+                                employeesService.InsertOfficeWorker(newOfficeWorker);
+                                Console.WriteLine("OfficeWorker creation successful!");
+                                break;
+                        }
+                        break;
+                    case "2":
+                        Console.WriteLine("1. Production Workers List");
+                        Console.WriteLine("2. Add ProductionWorker");
+                        string choiceProductionWorkers = Console.ReadLine();
+                        switch (choiceProductionWorkers)
+                        {
+                            case "0":
+                                //Main Menu
+                                break;
+                            case "1":
+                                //GetAllProductionWorkers
+                                var ProductionWorkerList = employeesService.GetProductionWorkersList();
+                                foreach (ProductionWorker productionWorker in ProductionWorkerList.Result)
+                                {
+                                    Console.WriteLine(productionWorker);
+                                }
+                                break;
+                            case "2":
+                                //InsertProductionWorker
+                                ProductionWorker newProductionWorker = new ProductionWorker();
+                                Console.WriteLine("Enter ProductionWorker name");
+                                string name = Console.ReadLine();
+                                Console.WriteLine("Enter ProductionWorker surName");
+                                string surName = Console.ReadLine();
+                                Console.WriteLine("Enter ProductionWorker age");
+                                int age = int.Parse(Console.ReadLine());
+                                Console.WriteLine("Enter ProductionWorker shift");
+                                string shift = Console.ReadLine();
+                                newProductionWorker = new ProductionWorker(name, surName, age, shift);
+                                employeesService.InsertProductionWorker(newProductionWorker);
+                                Console.WriteLine("ProductionWorker creation successful!");
+                                break;
+                        }
+                        break;
+                }
+            }
 
         }
 
